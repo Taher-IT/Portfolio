a48e78be82b8fdb39bdf85043b3ea49e876c68b5
Taher-IT <tah.abd.megid@gmail.com>
2025-03-31 08:23:18 +0200

    About Me Section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from "./hero/hero.component";
 import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeroComponent, HeaderComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
