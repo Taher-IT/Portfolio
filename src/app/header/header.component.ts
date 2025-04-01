45576587e8eddc2f18f50a13e4a03ed7ecd519c2
Taher-IT <tah.abd.megid@gmail.com>
2025-04-01 08:13:05 +0200

    Brake

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,32 @@
 import { Component, HostListener } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
   isScrolled: boolean = false;
+  currentSection: string = '';
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.currentSection = this.router.url.split('#')[1] || '';
+    });
+  }
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.documentElement.scrollTop || 0;
-    this.isScrolled = scrollPosition > 860;
+    this.isScrolled = window.scrollY > 730;
   }
 
+  isActive(section: string): boolean {
+    return this.currentSection === section;
+  }
 }
