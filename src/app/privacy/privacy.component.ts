ceb416fdddc2bf7f53d72c2a7a14d7c2a08ee9ca
Taher-IT <tah.abd.megid@gmail.com>
2025-06-04 22:03:33 +0200

    finished the translations + respsonsive: privacy and legal notice done

--- src/app/privacy/privacy.component.ts
+++ src/app/privacy/privacy.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { HeaderComponent } from "../header/header.component";
+import { TranslationService } from '../services/translation.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-privacy',
   standalone: true,
-  imports: [RouterLink, HeaderComponent],
+  imports: [RouterLink, HeaderComponent, CommonModule],
   templateUrl: './privacy.component.html',
   styleUrl: './privacy.component.scss'
 })
 export class PrivacyComponent implements OnInit {
+  constructor(public ts: TranslationService) { }
+
   ngOnInit() {
     window.scrollTo(0, 0);
   }
